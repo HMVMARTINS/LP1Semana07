7ef3a2879b195cb80e7ff8298fcf3c38e9aaf68e
HMVMARTINS <hugomvmartins90@gmail.com>
2024-04-19 00:05:13 +0100

    Exercício 5 MilitaryUnit Modificada

--- GameUnits/MilitaryUnit.cs
+++ GameUnits/MilitaryUnit.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace GameUnits
 {
     public class MilitaryUnit : Unit // Heranca!!
@@ -9,6 +11,32 @@ namespace GameUnits
         {                                                        // Unit(int, int)
             AttackPower = attackPower;
             XP = 0;
+        }
+
+        //Sobrescreve propriedade health para adicionar XP
+        public override int Health
+        {
+            get { return base.Health + XP;}
+            set { base.Health = value;}
+        }
+
+        //Sobrescreve propriedade Cost para adicionar XP
+        public override float Cost => AttackPower + XP;
+
+        //Método para atacar outra unidade
+        public void Attack(Unit u)
+        {
+            //Adiciona XP da própria unidade
+            XP++;
+
+            // Aplica dano igual ao AttackPower na unidade que foi atacada
+            u.Health -= AttackPower;
+
+            //Verifica se unidade alvo de ataque foi atacada
+            if (u.Health <=0)
+            {
+                Console.WriteLine($"A unidade {u.GetType().Name} foi derrotada!");
+            } 
         }
     }
 }
