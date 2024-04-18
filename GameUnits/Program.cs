3570f79cda8f99788532a7d0b028edc080b1b88b
HMVMARTINS <hugomvmartins90@gmail.com>
2024-04-19 00:43:03 +0100

    Exercício 5 Classe Program

--- GameUnits/Program.cs
+++ GameUnits/Program.cs
@@ -6,7 +6,23 @@ namespace GameUnits
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Instância MilitaryUnit
+            MilitaryUnit militaryUnit = new MilitaryUnit(2, 10, 4);
+
+            //Instância SettlerUnit
+            SettlerUnit settlerUnit = new SettlerUnit();
+
+            //Método Move
+            militaryUnit.move(3);
+            settlerUnit.move(2);
+
+            //Imprime Health e Cost da MilitaryUnit
+            Console.WriteLine($"Health da MilitaryUnit: {militaryUnit.Health}");
+            Console.WriteLine($"Cost da MilitaryUnit: {militaryUnit.Cost}");
+            
+            //Imprime Health e Cost da SettlerUnit
+            Console.WriteLine($"Health da SettlerUnit: {settlerUnit.Health}");
+            Console.WriteLine($"Cost da SettlerUnit: {settlerUnit.Cost}");
         }
     }
 }
